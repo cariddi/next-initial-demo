6401a37254b62f3407afa7f4933cb28d9f1e4d49
Lucas Cariddi <lucas.cariddi@compozelabs.com>
2023-01-07 18:28:14 +0100

    Adding Navbar component

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
+import { Navbar } from "../components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,9 @@ export default function HomePage() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
+
+			<Navbar />
+
 			<main className={styles.main}>
 				<div className={styles.description}>
 					<p>
